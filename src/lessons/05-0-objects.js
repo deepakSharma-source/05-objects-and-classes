6c3ab9e193bf5ed60c1457e91253cd8f17207b56
deepak sharma <137759005+deepakSharma-source@users.noreply.github.com>
2024-12-11 14:20:57 +0530

    object and classes done

--- src/lessons/05-0-objects.js
+++ src/lessons/05-0-objects.js
@@ -9,6 +9,9 @@
 // Define an object named babyPrototype with a method named speak that returns the string "goo goo ga ga"
 const babyPrototype = {
   // Your Code Here
+  speak: function() {
+    return "go ga ga";
+  },
 };
 
 // define a function named createBaby that takes two arguments name and age
@@ -17,22 +20,33 @@ const babyPrototype = {
 // the function should return the new object
 function createBaby(name, age) {
   // Your Code Here
+  let baby = Object.create(babyPrototype);
+  baby.name = name;
+  baby.age = age;
+  return baby;
 }
 
 // define a function named makeWarfaAndSpeak that creates a baby named Warfa with age 1
 // the function should return the result of calling the speak method on Warfa
 function makeBabyAndSpeakOne() {
   // Your Code Here
+  let barfa = createBaby("barfa", 1);
+  return barfa.speak();
 }
 
 // The baby is learning fast!
 // define a method on the babyPrototype named sayName that returns "My name is " + the name of the baby
 // Your Code Here
+babyPrototype.sayName = function() {
+ return "my name is " + this.name;
+};
 
 // define a function named makeBabyAndSayName that creates a baby named Warfa with age 1
 // the function should return the value of calling the sayName method on Warfa
 function makeBabyAndSayNameOne() {
+  let barfa = createBaby("barfa", 1);
   // Your Code Here
+  return barfa.sayName();
 }
 
 /**
@@ -47,26 +61,36 @@ function makeBabyAndSayNameOne() {
 // HINT: We wont't need to use Object.create here or to return anything
 function Baby(name, age) {
   // Your Code Here
+  this.name = name;
+  this.age = age;
 }
 
 // define a method on the Baby prototype named speak that returns the string "goo goo ga ga"
 Baby.prototype.speak = function () {
   // Your Code Here
+  return "go go gag ga ";
 };
 
 // define a function named makeBabyAndSpeak that creates a baby named Warfa with age 1
 // the function should return the value of calling the speak method on Warfa
 function makeBabyAndSpeakTwo() {
   // Your Code Here
+  let barfa = new baby("barfa", 1)
+  return barfa.speak();
 }
 
 // define a method on the Baby prototype named sayName that returns "My name is " + the name of the baby
 // Your Code Here
+baby.prototype.sayName() = function () {
+  return "my name is " + this.name();
+}
 
 // define a function named makeBabyAndSayName that creates a baby named Warfa with age 1
 // the function should return the value of calling the sayName method on Warfa
 function makeBabyAndSayNameTwo() {
   // Your Code Here
+  let barfa = new Baby("barfa", 1);
+   return barfa.sayName();
 }
 
 /**
@@ -80,14 +104,18 @@ function makeBabyAndSayNameTwo() {
 class BabyClass {
   constructor(name, age) {
     // Your Code Here
+    this.name = name;
+    this.age = age;
   }
 
   speak() {
     // Your Code Here
+    return "go gog ga"
   }
 
   sayName() {
     // Your Code Here
+    return "my name is " + this.name;
   }
 }
 
@@ -95,12 +123,16 @@ class BabyClass {
 // the function should return the value of calling the speak method on Warfa
 function makeBabyClassAndSpeak() {
   // Your Code Here
+  let barfa = new BabyClass("barfa", 1);
+  return barfa.speak();
 }
 
 // makeBabyClassAndSayName should create a baby named Warfa with age 1
 // the function should return the value of calling the sayName method on Warfa
 function makeBabyClassAndSayName() {
   // Your Code Here
+  let barfa = new BabyClass("barfa", 1);
+  return barfa.sayName();
 }
 
 /**
@@ -113,20 +145,32 @@ function makeBabyClassAndSayName() {
 
 // Define a ToddlerClass that extends BabyClass
 class ToddlerClass extends BabyClass {
-  constructor(name, age) {}
+  constructor(name, age) {
+    super(name, age);
+  }
 
   // Override the speak method
-  speak() {}
+  speak() {
+    return "i can talk now!";
+  }
 
   // New method specific to ToddlerClass
-  growUp() {}
+  growUp() {
+    this.age += 1;
+  }
 }
 
 // makeToddlerClassAndSpeak should create a toddler named Tina with age 2
 // the function should return the value of calling the speak method on Tina
-function makeToddlerClassAndSpeak() {}
+function makeToddlerClassAndSpeak() {
+  let tina = new ToddlerClass("tina", 2);
+  return tina.speak();
+}
 
 // makeToddlerClassAndGrowUp should create a toddler named Tina with age 2
 // the function should call the growUp method on Tina
 // the function should return the age of Tina
-function makeToddlerClassAndGrowUp() {}
+function makeToddlerClassAndGrowUp() {
+  let tina = new ToddlerClass("tina", 2);
+  return tina.age();
+}
--- src/webflow/index.js
+++ src/webflow/index.js
@@ -15,53 +15,108 @@ const animationDurationInMs = parseFloat(transitionDuration) * 1000;
  */
 class FlashCard {
   // Set the question and answer properties on the instance
-  constructor(question, answer) {}
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+  }
 
   // Update the text content of the uiCard element
-  updateUiCard() {}
+  updateUiCard() {
+    uiCard.textContent = this.question;
+  }
 
   // Toggle the text content of the uiCard element
   // Animate using the 'is-flipped' combo class
-  flip() {}
+  flip() {
+    uiCard.classList.toggle("is-flipped");
+    setTimeout(()=> {
+      uiCard.textContent =
+      uiCard.textContent === this.question ? this.answer : this.question;
+      uiCard.classList.toggle("is-flipped");
+    }, animationDurationInMs);
 }
 
 class FlashCardDeck {
   // Create a cards array property on the instance
   // Set the activeCard property on the instance
   // Call the loadCards method
-  constructor() {}
+  constructor() {
+    this._cards = [];
+    this.activeCard = 0;
+    this.loadCards();
+  }
 
   // Get all CMS data from the page
   // Parse the JSON data
   // Create a new FlashCard instance using the question and answer from the JSON data
   // Add the new FlashCard instance to the cards array
-  loadCards() {}
+  loadCards() {
+    const cmsData = document.querySelectorAll('[data-flashcard="info"]');
+    cmsData.forEach((card)=> {
+      const jsonData = JSON.parse(card.innerHTML);
+      const question = jsonData.question;
+      const answer = jsonData.answer;
+      this.addCard(new FlashCard(question, answer));
+    });
+      
+    
+  }
 
   // Add a card to the cards array
-  addCard(card) {}
+  addCard(card) {
+    this._cards.push(card);
+  }
 
   // Get a card from the cards array at the given index
-  getCard(index) {}
+  getCard(index) {
+    return this._cards[index];
+  }
 
   // Get the active card from the cards array
-  getActiveCard() {}
+  getActiveCard() {
+    return this.getCard(this.activeCard);
+  }
 
   // Get the number of cards in the cards array
-  getCardCount() {}
+  getCardCount() {
+    return this._cards.length;
+  }
 
   // Increment the activeCard property
   // If the activeCard is greater than or equal to the number of cards, set the activeCard to 0
   // Return the activeCard
-  getNextCard() {}
+  getNextCard() {
+    this.activeCard++;
+    if(this._cards >= this.getCardCount()) {
+      this.activeCard = 0;
+    }
+    return this._cards[this.activeCard];
+  }
 
   // Update the text content of the currentCount and totalCount elements
-  updateGameUi() {}
+  updateGameUi() {
+    currentCount.textContent = this.activeCard + 1;
+    totalCount.textContent = this.getCardCount();
+  }
 }
 
 function init() {
   // Create a new FlashCardDeck instance
+  const deck = new FlashCardDeck();
   // When the user clicks the next button, get the next card and update the ui
+  nextButton.addEventListener("click", ()=> {
+    const nextCard = deck.getNextCard();
+    nextCard.updateUiCard();
+    deck.updateGameUi();
+  });
   // Flip the active card when the user clicks on it
+  uiCard.addEventListener("click", ()=> {
+    const card = deck.getActiveCard();
+    card.flip();
+  });
   // set initial card and game ui
+  const card = deck.getActiveCard();
+  card.updateUiCard();
+  deck.updateGameUi();
 }
 init();
